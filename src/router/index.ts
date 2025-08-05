c4ffabdc33299bc488310d855e051dbd9637c065
baizhi958216 <1475289190@qq.com>
2025-08-05 14:04:16 +0800

    route: Change to hash route
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createWebHistory , createRouter } from 'vue-router'
+import { createWebHashHistory , createRouter } from 'vue-router'
 import Party from '../components/Party.vue'
 const routes = [
   {
@@ -8,7 +8,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
